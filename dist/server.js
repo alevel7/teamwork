3625a26971edeb2018cde452c33712e4451d82dd
kazeem taiwo <alevel7@gmail.com>
2020-03-04 14:45:42 +0100

    production file updated

--- dist/server.js
+++ dist/server.js
@@ -34,7 +34,7 @@ var app = (0, _express2.default)();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
